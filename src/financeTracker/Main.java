a545da6e199c8c1e7f1d9d01bbf4986a1a363d1c
mis1nput <reddragonz2005@protonmail.com>
2025-02-24 18:25:02 -0500

    Created Base Files

---
+++ src/financeTracker/Main.java
@@ -0,0 +1,9 @@
+package financeTracker;
+
+import financeTracker.controller.Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        new Controller().run();
+    }
+}
---
+++ src/financeTracker/controller/Controller.java
@@ -0,0 +1,9 @@
+package financeTracker.controller;
+
+public class Controller {
+
+    public void run(){
+        System.out.println("Welcome to Finance Tracker!");
+    }
+    
+}
